a3fb9d8a04ffd96fced5ebfbc592ee1276401943
Mona Naamani <134978312+MonaNaamani@users.noreply.github.com>
2023-07-26 03:19:20 +0300

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR"
+  };
+  stringArray = [];
+  for (let i=0 ; i < num; i++) {
+  stringArray.push(string);
+}
+  return stringArray.join('');
 };
+
+const number = Math.floor(Math.random() * 1000)
+
+repeatString("hey", number);
 
 // Do not edit below this line
 module.exports = repeatString;
