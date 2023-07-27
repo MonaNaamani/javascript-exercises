7f6a06e5418b71f1b84d83627a8f612f9c1350a9
Mona Naamani <134978312+MonaNaamani@users.noreply.github.com>
2023-07-27 04:22:33 +0300

    Complete Exercise 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -15,3 +15,13 @@ repeatString("hey", number);
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// const repeatString = function(string, num) {
+//   let newString = "";
+//   for (let i = 0 ; i < num; i++) {
+//     newString += string;
+// }
+//   return newString
+// }
+
+// repeatString("hey", 3);
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  stringArray = [];
+  string = word;
+  stringLength = string.length;
+    num = 1;
+  for ( i= stringLength ; i > 0; i-- ) {
+    stringArray.push(string.at(-num++))
+  }
+  return stringArray.join('');
 
 };
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
+
