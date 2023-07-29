c0c24c669d699858318099fea5794719d4286eed
Mona Naamani <134978312+MonaNaamani@users.noreply.github.com>
2023-07-29 04:34:57 +0300

    Add a solution that does not work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,36 @@
-const removeFromArray = function() {
+const removeFromArray = function(data, ...theArgs) {
+  function checkCondition() {
 
+  }
+    // for (const arg of theArgs) {
+    // }
+  
+  return data.filter(checkCondition)
 };
+
+removeFromArray([1, 2, 3, 4], 3);
+
+
+//  splice(targetElementIndex, 1)
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+let a = [1, 2, 3, 4, 3];
+let targetNumber = 3;
+
+let indexArray = a.map((element, index) => {
+  if (element === targetNumber) {
+    return index;
+  } else {
+    return null;
+  }
+});
+
+indexArray = [null, null, 2, null, 4]
+
+
+console.log(indicesWithNulls);
