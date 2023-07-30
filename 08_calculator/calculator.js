66b053e3f414dc562d85c525ffdfc5f128ed1af9
Mona Naamani <134978312+MonaNaamani@users.noreply.github.com>
2023-07-30 03:51:31 +0300

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,60 @@
-const add = function() {
-	
+const add = function(a,b) {return (a + b)};
+
+add(2, 6);
+
+const subtract = function(a,b) {return (a - b)};
+
+subtract(10, 4);
+
+const sum = function(array) {
+  totalSum = 0;
+  for (i=0 ; i < (array.length) ; i++) {
+  totalSum += array[i];  
+  }
+  return totalSum
 };
 
-const subtract = function() {
-	
+sum([1, 3, 5, 7, 9]);
+
+const multiply = function(array) {
+  totalMult = 1;
+  for ( let i = 0 ; i < (array.length) ; i++) {
+  totalMult *= array[i];  
+  }
+  return totalMult
 };
 
-const sum = function() {
-	
+multiply([2, 4, 6, 8, 10, 12, 14]);
+
+const power = function(a,b) {return Math.pow(a, b)};
+
+power(4, 3);
+
+const factorial = function(a) {
+  let totalFac = 1;
+  for ( let i = a ; i > 0; i--) {
+  totalFac *= i;
+}	
+  return totalFac
 };
 
-const multiply = function() {
+factorial(10);
 
-};
 
-const power = function() {
-	
-};
 
-const factorial = function() {
-	
-};
+
+// const factorial = function(a) {
+//   let totalFac = 1;
+//   for ( i=a ; i < 1; i--) {
+//   totalFac *= i;
+// }	
+// };
+
+// factorial(0);
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
